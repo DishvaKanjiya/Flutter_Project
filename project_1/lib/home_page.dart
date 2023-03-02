c450ff0b86e44096702a84ce096b0473ded9e55c
DishvaKanjiya <21010101097@darshan.ac.in>
2023-03-02 09:41:48 +0530

    Project_api

--- project_1/lib/home_page.dart
+++ project_1/lib/home_page.dart
@@ -7,72 +7,93 @@ class Home_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         body:  Stack(
-           fit: StackFit.expand,
-           children: [
-             Image.asset('images/download.jpg',
-                fit: BoxFit.fill,
-             ),
-             Container(
-               color: Colors.white.withOpacity(0.0),
-             ),
-             Column(
-              children: [
-                  Expanded(child: Container(),flex: 4,),
-                Row(
-                  children: [
-                    Expanded(child: Padding(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'images/download.jpg',
+            fit: BoxFit.fill,
+          ),
+          Container(
+            color: Colors.white.withOpacity(0.0),
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: Container(),
+                flex: 4,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Container(
-                        child: Text("Traveling made \n easy!",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 50),),
+                        child: Text(
+                          "Traveling made \n easy!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 50),
+                        ),
                       ),
-                    ),),
-                    Expanded(child: Container(),),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(child: Padding(
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Container(
-                        child: Text("Experience the world's best adventure around the world with us.",style: TextStyle(color: Colors.white,fontSize:30 ),),
+                        child: Text(
+                          "Experience the world's best adventure around the world with us.",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
                       ),
-                    ),),
-                    Expanded(child: Container(),),
-                  ],
-                 ),
-                 Expanded(
-                   child: TextButton(
-                     onPressed: (){},
-                       child: InkWell(
-                         onTap: () {
-                           Navigator.of(context).push(
-                             MaterialPageRoute(
-                               builder: (context) {
-                                 return Page_2();
-                               },
-                             ),
-                           );
-                         },
-                     child: Container(
-                       decoration: BoxDecoration(borderRadius: BorderRadius.circular(25),color: Colors.orange[200]),
-                       child: Padding(
-                         padding: const EdgeInsets.all(10.0),
-                         child: Text("     Let's Go     ",style: TextStyle(color: Colors.white),),
-                       ),
-                     ),
-                   )
-                 ),
-
-
-                 )
-              ],
-             )
-
-
-
-           ],
-         ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: TextButton(
+                    onPressed: () {},
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return Page_2();
+                            },
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: Colors.orange[200]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            "     Let's Go     ",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    )),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
+
 }
--- project_1/lib/page_2.dart
+++ project_1/lib/page_2.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:project_1/page_3..dart';
+import 'package:http/http.dart' as http;
 
 class Page_2 extends StatefulWidget {
   const Page_2({Key? key}) : super(key: key);
@@ -10,61 +13,63 @@ class Page_2 extends StatefulWidget {
 
 class _Page_2State extends State<Page_2> {
   @override
-  void initState() {
-    super.initState();
-  }
-  int selectedIndex = 0;
-  List<Widget> WidgetsList = [
-       Container(color: Colors.white,)
-  ];
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       body: Column(
         children: [
           Expanded(child: Row(
             children: [
-               Expanded(child: Text("    Let's   Discover",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30), ),flex: 4,),
-                Expanded(child:  Container(
-                  child: CircleAvatar(
-                    radius: 15,
-                    backgroundColor: Colors.grey,
-                    child: Icon(Icons.travel_explore),
-                  ),
-                ),),
+              Expanded(child: Text(
+                "    Let's   Discover", style: TextStyle(fontWeight: FontWeight
+                  .bold, fontSize: 30),), flex: 4,),
+              Expanded(child: Container(
+                child: CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.travel_explore),
+                ),
+              ),),
             ],
           ),),
+
           Row(
             children: [
               Expanded(child: Container(),),
               Expanded(child: Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
                     color: Colors.grey[350]),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Text(
-                        '   Search destination   ',
-                        style: TextStyle(color: Colors.black45,fontSize: 20),
-                        ),
-                      ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    '   Search destination   ',
+                    style: TextStyle(color: Colors.black45, fontSize: 20),
+                  ),
                 ),
+              ),
                 flex: 6,
               ),
               Expanded(child: Container(),),
             ],
           ),
+
           Expanded(child: Column(
             children: [
               Expanded(child: Row(
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(left: 30),
-                    child: Container(child: Text('Categories',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)),
+                    child: Container(child: Text('Categories', style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),)),
                   ),
                   Expanded(child: Padding(
                     padding: const EdgeInsets.only(right: 30),
-                    child: Text('See all',style: TextStyle(color: Colors.deepOrange,fontSize: 15, decoration:TextDecoration.underline),textAlign: TextAlign.right,),
+                    child: Text('See all', style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontSize: 15,
+                        decoration: TextDecoration.underline),
+                      textAlign: TextAlign.right,),
                   ),),
                 ],
               )),
@@ -73,41 +78,26 @@ class _Page_2State extends State<Page_2> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                     Column(
-                       children: [
-                         Padding(
-                           padding: const EdgeInsets.only(left: 20),
-                           child: Container(
-                             child:ClipRRect(
-                               borderRadius: BorderRadius.circular(10), // Image border
-                               child: SizedBox.fromSize(
-                                 size: Size.fromRadius(25), // Image radius
-                                 child: Image.asset('images/sydney.jpg', fit: BoxFit.cover),
-                               ),
-                             ),
-                           ),
-                         ),
-                         Container(child: Padding(
-                           padding: const EdgeInsets.only(left: 20,top: 8.0),
-                           child: Text('Sydney',style: TextStyle(fontSize: 13),),
-                         ),)
-                       ],
-                     ),
-
                     Column(
                       children: [
-                        Container(
-                          child:ClipRRect(
-                            borderRadius: BorderRadius.circular(10), // Image border
-                            child: SizedBox.fromSize(
-                              size: Size.fromRadius(25), // Image radius
-                              child: Image.asset('images/usa.jpg', fit: BoxFit.cover),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Container(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              // Image border
+                              child: SizedBox.fromSize(
+                                size: Size.fromRadius(25), // Image radius
+                                child: Image.asset(
+                                    'images/sydney.jpg', fit: BoxFit.cover),
+                              ),
                             ),
                           ),
                         ),
                         Container(child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text('Usa',style: TextStyle(fontSize: 13),),
+                          padding: const EdgeInsets.only(left: 20, top: 8.0),
+                          child: Text(
+                            'Sydney', style: TextStyle(fontSize: 13),),
                         ),)
                       ],
                     ),
@@ -115,17 +105,19 @@ class _Page_2State extends State<Page_2> {
                     Column(
                       children: [
                         Container(
-                          child:ClipRRect(
-                            borderRadius: BorderRadius.circular(10), // Image border
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            // Image border
                             child: SizedBox.fromSize(
                               size: Size.fromRadius(25), // Image radius
-                              child: Image.asset('images/japan.jpg', fit: BoxFit.cover),
+                              child: Image.asset(
+                                  'images/usa.jpg', fit: BoxFit.cover),
                             ),
                           ),
                         ),
                         Container(child: Padding(
                           padding: const EdgeInsets.all(8.0),
-                          child: Text('Japan',style: TextStyle(fontSize: 13),),
+                          child: Text('Usa', style: TextStyle(fontSize: 13),),
                         ),)
                       ],
                     ),
@@ -133,21 +125,42 @@ class _Page_2State extends State<Page_2> {
                     Column(
                       children: [
                         Container(
-                          child:ClipRRect(
-                            borderRadius: BorderRadius.circular(10), // Image border
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            // Image border
                             child: SizedBox.fromSize(
                               size: Size.fromRadius(25), // Image radius
-                              child: Image.asset('images/Paris.jpg', fit: BoxFit.cover),
+                              child: Image.asset(
+                                  'images/japan.jpg', fit: BoxFit.cover),
                             ),
                           ),
                         ),
                         Container(child: Padding(
                           padding: const EdgeInsets.all(8.0),
-                          child: Text('Paris',style: TextStyle(fontSize: 13),),
+                          child: Text('Japan', style: TextStyle(fontSize: 13),),
                         ),)
                       ],
                     ),
 
+                    Column(
+                      children: [
+                        Container(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            // Image border
+                            child: SizedBox.fromSize(
+                              size: Size.fromRadius(25), // Image radius
+                              child: Image.asset(
+                                  'images/Paris.jpg', fit: BoxFit.cover),
+                            ),
+                          ),
+                        ),
+                        Container(child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('Paris', style: TextStyle(fontSize: 13),),
+                        ),)
+                      ],
+                    ),
 
 
                     Column(
@@ -155,18 +168,20 @@ class _Page_2State extends State<Page_2> {
                         Padding(
                           padding: const EdgeInsets.only(right: 20),
                           child: Container(
-                            child:ClipRRect(
-                              borderRadius: BorderRadius.circular(10), // Image border
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              // Image border
                               child: SizedBox.fromSize(
                                 size: Size.fromRadius(25), // Image radius
-                                child: Image.asset('images/kenya.jpg', fit: BoxFit.cover),
+                                child: Image.asset(
+                                    'images/kenya.jpg', fit: BoxFit.cover),
                               ),
                             ),
                           ),
                         ),
                         Container(child: Padding(
-                          padding: const EdgeInsets.only(right: 20,top: 8.0),
-                          child: Text('Kenya',style: TextStyle(fontSize: 13),),
+                          padding: const EdgeInsets.only(right: 20, top: 8.0),
+                          child: Text('Kenya', style: TextStyle(fontSize: 13),),
                         ),)
                       ],
                     ),
@@ -176,7 +191,7 @@ class _Page_2State extends State<Page_2> {
                 ),
               )),
             ],
-          ),flex: 3,),
+          ), flex: 3,),
 
           Expanded(child: Padding(
             padding: const EdgeInsets.all(8.0),
@@ -184,90 +199,112 @@ class _Page_2State extends State<Page_2> {
               decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
                   color: Colors.grey[350]),
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Expanded(child: Center(child: Text('All',style: TextStyle(fontSize: 15),)),),
-                    Expanded(child: Container(decoration:BoxDecoration(borderRadius: BorderRadius.circular(30),color: Colors.white),child: Center(child: Text('Popular',style: TextStyle(fontSize: 15,color: Colors.deepOrange),))),),
-                    Expanded(child: Center(child: Text('News',style: TextStyle(fontSize: 15),)),),
-                    Expanded(child: Center(child: Text('More',style: TextStyle(fontSize: 15),))),
-                  ],
-                )
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(child: Center(child: Text(
+                        'All', style: TextStyle(fontSize: 15),)),),
+                      Expanded(child: Container(decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white),
+                          child: Center(child: Text('Popular', style: TextStyle(
+                              fontSize: 15, color: Colors.deepOrange),))),),
+                      Expanded(child: Center(child: Text(
+                        'News', style: TextStyle(fontSize: 15),)),),
+                      Expanded(child: Center(child: Text(
+                        'More', style: TextStyle(fontSize: 15),))),
+                    ],
+                  )
               ),
             ),
           ),),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: InkWell(onTap: () {
-                    Navigator.of(context).push(
-                    MaterialPageRoute(
-                    builder: (context) {
-                    return Page_3();
-                  },),);},child: Container(child: ClipRRect(borderRadius:BorderRadius.circular(10),child: Image.asset('images/Island.jpg'),),)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Container(child: ClipRRect(borderRadius:BorderRadius.circular(10),child: Image.asset('images/Jammu_kashmir.jpg')),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Container(child: ClipRRect(borderRadius:BorderRadius.circular(10),child: Image.asset('images/Cape_town.jpg')),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Container(child: ClipRRect(borderRadius:BorderRadius.circular(10),child: Image.asset('images/Disneyland.jpg')),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Container(child: ClipRRect(borderRadius:BorderRadius.circular(10),child: Image.asset('images/Melbourne.jpg')),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Container(child: ClipRRect(borderRadius:BorderRadius.circular(10),child: Image.asset('images/Skylines.jpg')),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Container(child: ClipRRect(borderRadius:BorderRadius.circular(10),child: Image.asset('images/Manali.jpg')),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Container(child: ClipRRect(borderRadius:BorderRadius.circular(10),child: Image.asset('images/London.jpg')),),
-                ),
-              ],
+
+          Container(
+            height: 250,
+            child: FutureBuilder<List<dynamic>>(
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(itemBuilder: (context, index) {
+                   return Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(65),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                              return Page_3();
+                            }));
+                          },
+                          child: Container(
+                            child: Image.asset(
+                                snapshot.data![index]['CityImages'].toString()),
+                          ),
+                        ),
+                      ),
+                    );
+                  }, itemCount: snapshot.data!.length,
+                  scrollDirection: Axis.horizontal,);
+                }
+                else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },future: getDetailFromApi(),
             ),
-          ),
+          )
+
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home,),
-            label: 'Home',
-            backgroundColor: Colors.cyan,
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.location_on),
-              label: 'Add'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              label: 'List'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.contact_page),
-              label: 'contact_page'
-          ),
-        ],
-        selectedItemColor: Colors.orange,
+
+
+      bottomNavigationBar: BottomAppBar(
+        child:
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: IconButton(icon: Icon(
+                Icons.home,
+                color: Colors.deepOrangeAccent,
+              ),
+                onPressed: () {},
+              ),
+            ),
+
+            IconButton(icon: Icon(
+              Icons.location_on,
+              color: Colors.blueGrey,
+            ),
+              onPressed: () {},
+            ),
+
+            IconButton(icon: Icon(
+              Icons.list,
+              color: Colors.blueGrey,
+            ),
+              onPressed: () {},
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(right: 15.0),
+              child: IconButton(icon: Icon(
+                Icons.contact_page,
+                color: Colors.blueGrey,
+              ),
+                onPressed: () {},
+              ),
+            ),
+          ],),
+        color: Colors.cyan,
       ),
-
-
-
-
     );
+  }
+
+  Future<List<dynamic>> getDetailFromApi() async {
+    http.Response res = await http.get(
+        Uri.parse('https://630d9abc109c16b9abe8d7c2.mockapi.io/Travel_icon'));
+
+    List<dynamic> jsonData = jsonDecode(res.body.toString());
+    return jsonData;
   }
 }
